0f24f94f8014e9b1f5f5e2031b82afec47155c81
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2016-06-06 15:11:23 +0200

    Add tester for lexer

---
+++ lexer_test.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <FlexLexer.h>
+#include "lexer.h" 
+
+int main() {
+    FlexLexer *lexer = new yyFlexLexer();
+    int lexical_symbol;
+    while ((lexical_symbol = lexer->yylex()) != EOI)
+        printf("lexical symbol: %s (%d)\n",
+                lexer->YYText(),
+                lexical_symbol);
+    return 0;
+}
